fe9ea6c3c5f16a9ae18efb402a033e26ac6545c0
Andie Fonderson <Andrea.Fonderson@IRIS.CO.UK>
2021-12-02 13:40:43 +0000

    Set the available moves for the rook.

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,20 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentPos = board.FindPiece(this);
+            List<Square> moves = new List<Square>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                moves.Add(Square.At(currentPos.Row, i));
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                moves.Add(Square.At(i, currentPos.Col));
+            }
+
+            return moves;
         }
     }
 }
