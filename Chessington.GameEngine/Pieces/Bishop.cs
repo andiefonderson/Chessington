20a96236e63e255e7d9e66e6a57356a05d2f828e
Andie Fonderson <Andrea.Fonderson@IRIS.CO.UK>
2021-12-02 14:15:19 +0000

    Test 4 now works - added in bishop's possible movements.

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,28 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> moves = new List<Square>();
+            Square currentPos = board.FindPiece(this);
+
+            for (int i = 1; i < 8; i++)
+            {
+                Square newPos = Square.At(i, i);
+                if(newPos != currentPos)
+                {
+                    moves.Add(Square.At(i, i));
+                }                
+            }
+
+            for (int i = 1; i < 8; i++)
+            {
+                Square newPos = Square.At(i, 8 - i);
+                if(newPos != currentPos)
+                {
+                    moves.Add(newPos);
+                }                
+            }
+
+            return moves;
         }
     }
 }
